e5e683b67a55f6a4b35f175877f3035ff4f52bc5
arinzepaschal791-code <arinzepaschal791@gmail.com>
2026-01-23 01:07:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# Homepage route
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# REMOVE this route if you are not using HTML
-# Flask does NOT support .tsx files
-# @app.route("/header")
-# def header():
-#     return render_template("Header.tsx")
+# Admin Panel route
+@app.route('/admin')
+def admin_panel():
+    return render_template('admin.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
